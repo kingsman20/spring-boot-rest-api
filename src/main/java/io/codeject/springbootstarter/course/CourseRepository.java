3e06de12154f72254408c9589e430c7c58b79622
kingsman20 <kingsley@nextravel.com>
2019-11-04 13:41:15 +0100

    Initial commit

---
+++ src/main/java/io/codeject/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,11 @@
+package io.codeject.springbootstarter.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String>{
+	
+	public List<Course> findByTopicId(String topicId);
+
+}
